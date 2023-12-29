4c75ff70bcb8bcf89213ab42e2894c26d7aecb93
Alejandro Ramírez Peralta <alejandroramirezperalta@MacBook-Pro-de-Alejandro.local>
2023-12-28 21:14:07 -0400

    changes important

--- dist/components/ModalComponents.d.ts
+++ dist/components/ModalComponents.d.ts
@@ -1,15 +1,15 @@
 import React from 'react';
 interface sizeProps {
-    md: 'sm:max-w-md';
-    lg: 'sm:max-w-lg';
-    xl: 'sm:max-w-xl';
-    full: 'sm:max-w-4xl';
+    'md': 'sm:max-w-md';
+    'lg': 'sm:max-w-lg';
+    'xl': 'sm:max-w-xl';
+    'full': 'sm:max-w-4xl';
 }
 interface ModalProps {
     isOpen: boolean;
     title: string;
     onClose: () => void;
-    size: sizeProps;
+    size?: sizeProps;
     children: any;
 }
 declare const ModalComponents: React.FC<ModalProps>;
--- src/components/ModalComponents.tsx
+++ src/components/ModalComponents.tsx
@@ -28,7 +28,7 @@ const ModalComponents: React.FC<ModalProps> = (props: ModalProps) => {
                             <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
                                 <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                                 <div
-                                    className={`relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full ${props.size ? props.size : 'sm:max-w-sm'}`}
+                                    className={`relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-md`}
                                 >
                                     <div className="flex justify-between items-center mb-2">
                                     <h2 className="text-lg font-semibold">{props.title}</h2>

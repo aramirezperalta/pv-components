1bc1e120c8777177cf6078f36ee8cc9230542328
Alejandro Ramírez Peralta <alejandroramirezperalta@MacBook-Pro-de-Alejandro.local>
2023-12-28 20:51:12 -0400

    changes important

--- src/components/ModalComponents.tsx
+++ src/components/ModalComponents.tsx
@@ -1,49 +1,65 @@
 import React from 'react';
 
+interface sizeProps {
+    md: 'sm:max-w-md',
+    lg: 'sm:max-w-lg',
+    xl: 'sm:max-w-xl',
+    full: 'sm:max-w-4xl',
+}
+
 interface ModalProps {
     isOpen: boolean;
+    title: string;
     onClose: () => void;
+    size: sizeProps;
     children: any;
 }
 
 const ModalComponents: React.FC<ModalProps> = (props: ModalProps) => {
-    React.useEffect(() => {
-        if (props.isOpen) {
-            document.body.style.overflow = 'hidden'; // Evita que el fondo de la página se desplace cuando el modal está abierto
-        } else {
-            document.body.style.overflow = 'unset'; // Restaura el comportamiento de desplazamiento normal
-        }
-
-        return () => {
-            document.body.style.overflow = 'unset'; // Asegura que el comportamiento de desplazamiento se restablezca cuando el componente se desmonta
-        };
-    }, [props.isOpen]);
 
     return (
         <>
-            <div
-            className={`fixed inset-0 flex p-2 items-start justify-center z-50 ${
-                props.isOpen ? 'block' : 'hidden'
-            }`}
-            >
-                <div
-                    className="fixed inset-0 bg-black opacity-50"
-                    onClick={props.onClose}
-                />
-                <div className="bg-white rounded-lg p-1 z-50 overflow-auto">
-                    <div className={'flex justify-end pb-4'}>
-                        <button
-                            className="hover:bg-gray-100 flex py-1 px-1 rounded"
-                            onClick={props.onClose}
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-slate-700">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
-                        </button>
-                    </div>
-                    {props.children}
-                </div>
-            </div>
+            {
+                props.isOpen ?
+                    <>
+                        <div className="relative z-50" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+                            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+
+                            <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                                <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                                <div
+                                    className={`relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full ${props.size ? props.size : 'sm:max-w-sm'}`}
+                                >
+                                    <div className="flex justify-between items-center mb-2">
+                                    <h2 className="text-lg font-semibold">{props.title}</h2>
+                                    <button
+                                        type="button"
+                                        onClick={props.onClose}
+                                        className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                    >
+                                        <span className="sr-only">Close</span>
+                                        <svg
+                                        className="h-6 w-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke-width="1.5"
+                                        stroke="currentColor"
+                                        aria-hidden="true"
+                                        >
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                        </svg>
+                                    </button>
+                                    </div>
+                                    <hr />
+                                    <div>
+                                        {props.children}
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </> : null
+            }
         </>
     );
 };
